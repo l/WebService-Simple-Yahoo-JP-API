8d35c66cd3a08fdb9b8ba60ef7106cb870b0647e
l <l@github.com>
2010-08-09 14:51:10 +0900

    initial commit

---
+++ lib/WebService/Simple/Yahoo/JP/API.pm
@@ -0,0 +1,33 @@
+package WebService::Simple::Yahoo::JP::API;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::Simple::Yahoo::JP::API -
+
+=head1 SYNOPSIS
+
+  use WebService::Simple::Yahoo::JP::API;
+
+=head1 DESCRIPTION
+
+WebService::Simple::Yahoo::JP::API is
+
+=head1 AUTHOR
+
+AYANOKOUZI, Ryuunosuke E<lt>i38w7i3@yahoo.co.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
