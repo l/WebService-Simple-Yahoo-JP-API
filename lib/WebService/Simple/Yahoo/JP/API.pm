d5d62c2fc7be2e71e94a16a3d69f42f535ab3952
l <l@github.com>
2010-11-18 16:02:01 +0900

    add requre section to Makefile.PL for test.

--- lib/WebService/Simple/Yahoo/JP/API.pm
+++ lib/WebService/Simple/Yahoo/JP/API.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use URI;
 use base qw(WebService::Simple);
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 __PACKAGE__->config(
 		base_url => 'http://yahooapis.jp/',
 		);
