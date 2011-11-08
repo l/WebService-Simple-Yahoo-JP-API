86b6841ce7c2b5bb705583cc855833d535d80853
l <l@github.com>
2011-11-08 18:55:51 +0900

    version update

--- lib/WebService/Simple/Yahoo/JP/API.pm
+++ lib/WebService/Simple/Yahoo/JP/API.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use URI;
 use base qw(WebService::Simple);
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 __PACKAGE__->config(
 		base_url => 'http://yahooapis.jp/',
 		);
